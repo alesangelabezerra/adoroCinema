bb1a83ebc6913dba2da696c2b5bb6e2b842c0949
Alesangela Bezerra <48522966+alesangelabezerra@users.noreply.github.com>
2023-12-26 08:53:54 -0300

    Add files via upload

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export default function Footer() {
+  return (
+    <Container>
+      <strong>Music</strong>
+    </Container>
+  );
+}
